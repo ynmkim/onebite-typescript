84c28972772838c9973faaf48750c6042050871b
김윤미 <younmeet@gmail.com>
2024-03-07 19:05:25 +0900

    Refactor: useReducer로 상태관리 로직 분리하기

--- section11/src/App.tsx
+++ section11/src/App.tsx
@@ -1,26 +1,51 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useReducer, useRef } from 'react';
 import './App.css';
 import Editor from './components/Editor';
 import TodoItem from './components/TodoItem';
 import { Todo } from './types';
 
+type Action =
+  | {
+      type: 'CREATE';
+      data: {
+        id: number;
+        content: string;
+      };
+    }
+  | {
+      type: 'DELETE';
+      id: number;
+    }; // 서로소 유니온 타입
+
+function reducer(state: Todo[], action: Action) {
+  switch (action.type) {
+    case 'CREATE':
+      return [...state, action.data];
+    case 'DELETE':
+      return state.filter((it) => it.id !== action.id);
+  }
+}
+
 function App() {
-  const [todoList, setTodoList] = useState<Todo[]>([]);
+  const [todoList, dispatch] = useReducer(reducer, []);
 
   const idRef = useRef(0);
 
   const onCreate = (text: string) => {
-    setTodoList([
-      ...todoList,
-      {
+    dispatch({
+      type: 'CREATE',
+      data: {
         id: idRef.current++,
         content: text,
       },
-    ]);
+    });
   };
 
   const onDelete = (id: number) => {
-    setTodoList(todoList.filter((todo) => todo.id !== id));
+    dispatch({
+      type: 'DELETE',
+      id: id,
+    });
   };
 
   useEffect(() => {
